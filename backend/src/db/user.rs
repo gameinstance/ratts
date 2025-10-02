e6c50ef221f57c9cf20423185f3467019c0f9f91
Bogdan A. <bogdan@gameinstance.com>
2025-10-02 20:36:14 +0200

    backend: improved database error precision
    Signed-off-by: Bogdan A. <bogdan@gameinstance.com>

--- backend/src/db/user.rs
+++ backend/src/db/user.rs
@@ -8,8 +8,7 @@ pub async fn get_by_id(pool: &PgPool, id: i64) -> AppResult<User> {
 	sqlx::query_as::<_, User>("SELECT * FROM authentication.\"user\" WHERE id = $1")
 		.bind(id)
 		.fetch_optional(pool)
-		.await
-		.map_err(AppError::DbError)?
+		.await?
 		.ok_or(AppError::NotFound)
 }
 
@@ -17,16 +16,16 @@ pub async fn get_private_by_email(pool: &PgPool, email: &str) -> AppResult<Priva
 	sqlx::query_as::<_, PrivateUser>("SELECT * FROM authentication.\"user\" WHERE email = $1")
 		.bind(email)
 		.fetch_optional(pool)
-		.await
-		.map_err(AppError::DbError)?
+		.await?
 		.ok_or(AppError::NotFound)
 }
 
 pub async fn create(pool: &PgPool, new_user: NewUser) -> AppResult<User> {
-	sqlx::query_as::<_, User>("INSERT INTO authentication.\"user\" (email, password_hash) VALUES ($1, $2) RETURNING *")
+	let record = sqlx::query_as::<_, User>("INSERT INTO authentication.\"user\" (email, password_hash) VALUES ($1, $2) RETURNING *")
 		.bind(new_user.email)
 		.bind(new_user.password_hash)
 		.fetch_one(pool)
-		.await
-		.map_err(AppError::DbError)
+		.await?;
+
+	Ok(record)
 }
